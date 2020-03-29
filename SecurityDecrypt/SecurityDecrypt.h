5739994c7a39beca59f667a98d9640a6d106a017
virgilius <virgilius.santos@acad.pucrs.br>
2020-03-29 01:26:48 -0300

    Initial Commit

---
+++ SecurityDecrypt/SecurityDecrypt.h
@@ -0,0 +1,19 @@
+//
+//  SecurityDecrypt.h
+//  SecurityDecrypt
+//
+//  Created by Virgilius Santos on 29/03/20.
+//  Copyright © 2020 Virgilius Santos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SecurityDecrypt.
+FOUNDATION_EXPORT double SecurityDecryptVersionNumber;
+
+//! Project version string for SecurityDecrypt.
+FOUNDATION_EXPORT const unsigned char SecurityDecryptVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecurityDecrypt/PublicHeader.h>
+
+
