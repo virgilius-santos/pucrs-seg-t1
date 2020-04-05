e886bbc761d259f34586b70d5f9400672b96a066
virgilius <virgilius.santos@acad.pucrs.br>
2020-04-05 12:09:17 -0300

    udpate tests

--- SecurityDecrypt/Read.swift
+++ SecurityDecrypt/Read.swift
@@ -2,10 +2,28 @@
 import Foundation
 
 class Read {
-    func read(_ file: String, format: String) throws -> String? {
+    static func read(_ file: String, format: String) -> String? {
         if let path = bundle.path(forResource: file, ofType: format) {
-            return try String(contentsOfFile: path, encoding: .utf8)
+            return try? String(contentsOfFile: path, encoding: .utf8)
         }
         return nil
+    }
+    
+    static func write(_ file: String, format: String, content: String) -> Bool {
+        if let path = bundle.path(forResource: file, ofType: format) {
+            
+            let url = URL(fileURLWithPath: path)
+            
+            if
+                let file = try? FileHandle(forUpdating: url),
+                let data = content.data(using: .utf8)
+            {
+                
+                file.write(data)
+                file.closeFile()
+                return true
+            }
+        }
+        return false
     }
 }
